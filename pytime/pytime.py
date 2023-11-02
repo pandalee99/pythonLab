f1cdbd7e82ee369d66ba955088972c38d6b4c717
pandalee99 <1162953505@qq.com>
2023-11-02 16:46:55 +0800

    time

--- pytime/pytime.py
+++ pytime/pytime.py
@@ -3,5 +3,13 @@
 
 import time
 
+# !/usr/bin/python3
+
+ticks = time.time()
+print("当前时间戳为:", ticks)
+
+localtime = time.localtime(time.time())
+print("本地时间为 :", localtime)
+
 localtime = time.asctime(time.localtime(time.time()))
-print(localtime)
+print("本地时间为 :", localtime)
