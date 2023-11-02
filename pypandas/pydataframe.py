0e46fead25b6534e51ed684bdfe7fbf5c7c1a399
pandalee99 <1162953505@qq.com>
2023-11-02 19:28:36 +0800

    dataframe

---
+++ pypandas/pydataframe.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+
+data = {'Site':['Google', 'Runoob', 'Wiki'], 'Age':[10, 12, 13]}
+
+df = pd.DataFrame(data)
+
+print (df)
+
+
+data = [{'a': 1, 'b': 2},{'a': 5, 'b': 10, 'c': 20}]
+
+df = pd.DataFrame(data)
+
+print (df)
+
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+# 数据载入到 DataFrame 对象
+df = pd.DataFrame(data)
+
+# 返回第一行
+print(df.loc[0])
+# 返回第二行
+print(df.loc[1])
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+# 数据载入到 DataFrame 对象
+df = pd.DataFrame(data)
+
+# 返回第一行和第二行
+print(df.loc[[0, 1]])
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+df = pd.DataFrame(data, index = ["day1", "day2", "day3"])
+
+print(df)
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+df = pd.DataFrame(data, index = ["day1", "day2", "day3"])
+
+# 指定索引
+print(df.loc["day2"])
